b057118c936fffe339f296327d1b82d99bd231fc
Luan Andrade <luansandrade92@hotmail.com>
2021-04-07 22:33:23 -0300

    Create basic layout with future data

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
-
+import 'package:conversor_moedas/main.dart';
 import 'package:flutter/material.dart';
-
 
 class Home extends StatefulWidget {
   @override
@@ -8,12 +7,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 24.0),
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Erro ao carregar dados :(",
+                        style: TextStyle(color: Colors.amber, fontSize: 24.0)),
+                  );
+                } else {
+                  return Container(
+                    color: Colors.white,
+                  );
+                }
+            }
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const api_key = String.fromEnvironment('API_KEY', defaultValue: 'API_KEY');
 const request = "https://api.hgbrasil.com/finance?format=json&key=" + api_key;
 
 void main() async {
-
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
+  print(await getData());
   runApp(Home());
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
