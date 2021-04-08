7c75b83dcff587ea78993454146654bee525c6fc
Luan Andrade <luansandrade92@hotmail.com>
2021-04-07 22:14:43 -0300

    request currency data from hgbrasil api

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,20 @@
 
 import 'package:flutter/material.dart';
 
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Container(
+
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'home.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
-void main() {
+const api_key = String.fromEnvironment('API_KEY', defaultValue: 'API_KEY');
+const request = "https://api.hgbrasil.com/finance?format=json&key=" + api_key;
+
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)['results']['currencies']['USD']);
   runApp(Home());
 }
