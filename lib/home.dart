b9d5130b997b1e74e02c2680a73739da1886fade
Luan Andrade <luansandrade92@hotmail.com>
2021-04-08 23:13:02 -0300

    create fields for real, dolar and euro

--- lib/home.dart
+++ lib/home.dart
@@ -7,9 +7,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
@@ -36,8 +50,44 @@ class _HomeState extends State<Home> {
                         style: TextStyle(color: Colors.amber, fontSize: 24.0)),
                   );
                 } else {
-                  return Container(
-                    color: Colors.white,
+                  dolar = snapshot.data['results']['currencies']['USD']['buy'];
+                  euro = snapshot.data['results']['currencies']['EUR']['buy'];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150.0,
+                          color: Colors.amber,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Reais",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "R\$"),
+                          style: TextStyle(color: Colors.amber, fontSize: 24.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Dolar",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "US\$"),
+                          style: TextStyle(color: Colors.amber, fontSize: 24.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Euro",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "€"),
+                          style: TextStyle(color: Colors.amber, fontSize: 24.0),
+                        )
+                      ],
+                    ),
                   );
                 }
             }
